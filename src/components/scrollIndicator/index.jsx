7abc0e5e2a0179f51b280f9274eb512c7fe2cd40
AjitKumar <ajit.kumar@zenesys.com>
2024-07-25 18:46:04 +0530

    scrollindicator

--- src/components/scrollIndicator/index.jsx
+++ src/components/scrollIndicator/index.jsx
@@ -1,16 +1,19 @@
+/** @format */
+
 import React, { useEffect, useState } from "react";
 
 export default function ScrollIndicator({ url }) {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   async function fetchData(getUrl) {
     try {
       setLoading(true);
       const response = await fetch(getUrl);
       const data = await response.json();
-    //   console.log("data", data);
+      //   console.log("data", data);
       if (data && data.products && data.products.length > 0) {
         setProducts(data.products);
         setLoading(false);
@@ -20,22 +23,37 @@ export default function ScrollIndicator({ url }) {
     } catch (e) {
       console.log(e);
       setErrorMessage(e.message);
-    } 
+    }
   }
 
   useEffect(() => {
     fetchData(url);
   }, [url]);
 
+function handleScrollPercentage(){
+    console.log(
+        document.body.scrollTop,
+        document.documentElement.scrollTop,
+        document.documentElement.scrollHeight,
+        document.documentElement.clientHeight
+    );
+}
+
+useEffect(()=>{
+     window.addEventListener('scroll',handleScrollPercentage)
+     return ()=>{
+        window.removeEventListener('scroll', ()=>{})
+     }
+},[])
 
   return (
     <div>
       <h4>ScrollIndicator</h4>
       {loading && <p>Loading...</p>}
       {errorMessage && <p>{errorMessage}</p>}
-      {products.map((product, index) => (
-        <p key={index}>{product.title}</p>
-      ))}
+      {products && products.length > 0
+        ? products.map((product, index) => <p key={index}>{product.title}</p>)
+        : null}
     </div>
   );
 }
