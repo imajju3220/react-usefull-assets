705f7afb14e307a372a59b7a294c7c982e6f76de
AjitKumar <ajit.kumar@zenesys.com>
2024-08-12 17:18:34 +0530

    scrollindicator

--- src/components/scrollIndicator/index.jsx
+++ src/components/scrollIndicator/index.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React, { useEffect, useState } from "react";
+import styles from './scroll.module.css'
 
 export default function ScrollIndicator({ url }) {
   const [products, setProducts] = useState([]);
@@ -13,7 +14,7 @@ export default function ScrollIndicator({ url }) {
       setLoading(true);
       const response = await fetch(getUrl);
       const data = await response.json();
-      //   console.log("data", data);
+      console.log("data", data);
       if (data && data.products && data.products.length > 0) {
         setProducts(data.products);
         setLoading(false);
@@ -31,23 +32,38 @@ export default function ScrollIndicator({ url }) {
   }, [url]);
 
 function handleScrollPercentage(){
-    console.log(
-        document.body.scrollTop,
-        document.documentElement.scrollTop,
-        document.documentElement.scrollHeight,
-        document.documentElement.clientHeight
-    );
+    // console.log(
+    //     document.body.scrollTop,
+    //     document.documentElement.scrollTop,
+    //     document.documentElement.scrollHeight,
+    //     document.documentElement.clientHeight
+    // );
+
+    const howMuchScrolled = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = 
+    document.documentElement.scrollHeight - 
+    document.documentElement.clientHeight;
+
+    setScrollPercentage((howMuchScrolled / height)*100)
 }
 
 useEffect(()=>{
      window.addEventListener('scroll',handleScrollPercentage)
      return ()=>{
         window.removeEventListener('scroll', ()=>{})
-     }
+     }     
 },[])
 
+console.log('scrollPercentage', scrollPercentage);
   return (
     <div>
+      <div className={styles.indicator}>
+        <div className={styles.posRelative}>
+        <div className={styles.count}>{scrollPercentage} %</div>
+        <div className={styles.innerBox} style={{width: `${scrollPercentage}%`}}>          
+        </div>
+        </div>
+      </div>
       <h4>ScrollIndicator</h4>
       {loading && <p>Loading...</p>}
       {errorMessage && <p>{errorMessage}</p>}
